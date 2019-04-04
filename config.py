440e47dcc7259db7ef4cd6797d913117ae0a6865
jonjump <daviesjpuk@gmail.com>
2019-04-04 12:54:31 +0100

    removed config.py, yet another paypal format

--- config.py
+++
@@ -1,6 +0,0 @@
-HOST = "localhost"
-PORT = "5000"
-SQLITE3DB = "sdm.db" # name for the sqlite database 
-PRODUCTION = True # set True for dev environment
-APPSECRETKEY = "aNotVerySecretKey" # definitely change this
-OIDC_GOOGLE_APPS_DOMAIN = "mygoogledomain.com" # google apps domain to autneticate users against
